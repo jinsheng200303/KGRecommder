9bb0773b99db4dcd0cad40c9158013c4a5b13efe
jinsheng <2247991411@qq.com>
2023-05-15 23:20:59 +0800

    改写了配置文件，变为.yaml文件。同时，配置多环境启动，添加了"开发环境"和"生产环境"。

--- src/main/java/cn/hunnu/recommender/mytest/rest/MyTestController.java
+++ src/main/java/cn/hunnu/recommender/mytest/rest/MyTestController.java
@@ -2,6 +2,7 @@ package cn.hunnu.recommender.mytest.rest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //将数据以json形式返回
 public class MyTestController {
 
+    @Value("${text}")
+    private String text;
+
     @GetMapping("/test")
     @ApiOperation(value = "测试",notes = "测试")
     public String test(){
         return "Hello World123!!!";
+    }
+
+    @GetMapping("/text")
+    public String text(){
+        return text;
     }
 }
