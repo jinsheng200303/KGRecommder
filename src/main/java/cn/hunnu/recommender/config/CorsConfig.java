cb8b09718d2a461a25f68c4e9b9d454d4ff22e39
czj <928342792@qq.com>
2023-07-01 14:33:20 +0800

    1.取消了后端跨域解决

--- src/main/java/cn/hunnu/recommender/config/CorsConfig.java
+++ src/main/java/cn/hunnu/recommender/config/CorsConfig.java
@@ -6,21 +6,38 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@Configuration
-public class CorsConfig {//解决跨域问题
-
-    // 当前跨域请求最大有效时长。这里默认1天
-    private static final long MAX_AGE = 24 * 60 * 60;
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 1 设置访问源地址
-        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
-        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
-        corsConfiguration.setMaxAge(MAX_AGE);
-        source.registerCorsConfiguration("/**", corsConfiguration); // 4 对接口配置跨域设置
-        return new CorsFilter(source);
-    }
-}
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+//
+//@Configuration
+//public class CorsConfig {//解决跨域问题
+//
+//    // 当前跨域请求最大有效时长。这里默认1天
+//    private static final long MAX_AGE = 24 * 60 * 60;
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 1 设置访问源地址
+//        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
+//        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
+//        corsConfiguration.setMaxAge(MAX_AGE);
+//        source.registerCorsConfiguration("/**", corsConfiguration); // 4 对接口配置跨域设置
+//        return new CorsFilter(source);
+//    }
+//}
+//@Configuration
+//public class CorsConfig implements Filter {
+//    @Override
+//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) res;
+//        response.setHeader("Access-Control-Allow-Origin","*");
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//        chain.doFilter(req, res);
+//    }
+//}
--- src/main/java/cn/hunnu/recommender/config/SpringMvcConfig.java
+++ src/main/java/cn/hunnu/recommender/config/SpringMvcConfig.java
@@ -65,7 +65,6 @@ public class SpringMvcConfig implements WebMvcConfigurer {
                         "/configuration/security"
 
                 );
-
     }
 
 
