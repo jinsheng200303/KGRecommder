718febabbf55464fa3ba1ca228c59248958877a0
jinsheng <2247991411@qq.com>
2023-07-04 14:33:07 +0800

    删除graph包中冗余代码文件

--- src/main/java/cn/hunnu/recommender/graph/ontology/InfoOntology.java
+++
@@ -1,7 +0,0 @@
-package cn.hunnu.recommender.graph.ontology;
-
-public class InfoOntology {
-    public static final String CLA_POLITICAL = "political";
-    public static final String CLA_GENDER = "gender";
-    public static final String CLA_DIC_DATA = "dic_data";
-}
