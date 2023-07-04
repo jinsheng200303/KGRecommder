7d1514291443a72cbb63b8e6217b39990e7ae371
jinsheng <2247991411@qq.com>
2023-07-04 10:03:27 +0800

    修改拦截器配置，将"/*"改为"/**"

--- src/main/java/cn/hunnu/recommender/graph/ontology/InfoOntology.java
+++ src/main/java/cn/hunnu/recommender/graph/ontology/InfoOntology.java
@@ -1,6 +1,6 @@
 package cn.hunnu.recommender.graph.ontology;
 
-public class InforOntology {
+public class InfoOntology {
     public static final String CLA_POLITICAL = "political";
     public static final String CLA_GENDER = "gender";
     public static final String CLA_DIC_DATA = "dic_data";
